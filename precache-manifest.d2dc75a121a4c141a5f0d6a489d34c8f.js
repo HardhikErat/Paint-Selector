19ba1981064c84fbba074bb5d829ac83b574cf2f
HardhikErat <binduserat@gmail.com>
2021-01-28 12:54:57 +0530

    Updates

---
+++ precache-manifest.d2dc75a121a4c141a5f0d6a489d34c8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28df2e3dc59d5834287a9f9ca30f3165",
+    "url": "/Paint-Selector/index.html"
+  },
+  {
+    "revision": "5b3af0b4f95ece125f3d",
+    "url": "/Paint-Selector/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "2d655c6be1566d4253e5",
+    "url": "/Paint-Selector/static/css/main.45bb13b8.chunk.css"
+  },
+  {
+    "revision": "5b3af0b4f95ece125f3d",
+    "url": "/Paint-Selector/static/js/2.a00f0603.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/Paint-Selector/static/js/2.a00f0603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d655c6be1566d4253e5",
+    "url": "/Paint-Selector/static/js/main.87084a83.chunk.js"
+  },
+  {
+    "revision": "eb7c81607027930f12b9",
+    "url": "/Paint-Selector/static/js/runtime-main.0f1230ab.js"
+  },
+  {
+    "revision": "ecea6c3aaafc85d5c9984a0bb51d738c",
+    "url": "/Paint-Selector/static/media/logo.ecea6c3a.png"
+  }
+]);
